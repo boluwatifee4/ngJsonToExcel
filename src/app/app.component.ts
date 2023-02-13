e6e4eea7abe3e664a24c5bd57cd5fb75e7a03676
boluwatifee4 <boluwatifee4@gmail.com>
2023-02-13 23:44:37 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { ExcelServiceService } from './excel-service.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngJson2Excel';
+  apiJsonResponseData: any;
+
+  constructor(
+    private excelService: ExcelServiceService,
+    private http: HttpClient
+  ){
+   
+  }
+
+  getDummyData(): Observable<any> {
+   return  this.http.get('https://jsonplaceholder.typicode.com/posts')
+ } 
+ 
+  exportEcel(): void {
+
+  }
+
+
 }
---
+++ src/app/excel-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+const EXCEL_EXTENSION = '.xlsx';
+@Injectable({
+  providedIn: 'root'
+})
+export class ExcelServiceService {
+
+  constructor() { }
+
+  public exportToExcel(element: any, fileName: string): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(element);
+    // generate workbook and add the worksheet
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, ws, 'Sheet1');
+    // save to file
+    XLSX.writeFile(workbook, `${fileName}${EXCEL_EXTENSION}`);
+
+  }
+  
+}
